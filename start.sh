fe03c7dc3e926e27e11c7d05440444233e9ce01e
Hein <heinhtetaung.itlife@gmail.com>
2019-01-13 23:50:24 +0630

    start.sh finished
    bash start.sh for no docker
    bash start.sh -d for docker

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+if [ "$1" != "" ]; then
+if [ "$1" == "-d" ]; then
+docker run --rm --interactive --tty \
+--volume $PWD:/app \
+composer install
+docker-compose exec app php artisan key:generate
+#run unit test
+docker-compose exec app vendor/bin/phpunit
+docker-compose exec app php artisan migrate:refresh
+fi	
+else
 composer install
 php artisan key:generate
+#run unit test
+vendor/bin/phpunit
 php artisan migrate:refresh
+fi
